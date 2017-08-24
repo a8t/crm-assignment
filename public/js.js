c6967235e0684ba5b0e971d3746fbbcb07c656d3
Andy Tran <andy8tran@gmail.com>
2017-08-24 16:32:14 -0400

    added more styles

--- public/js.js
+++ public/js.js
@@ -51,3 +51,28 @@ $(document).click(function(e) {
         $('.collapse').collapse('hide');
     }
 });
+
+
+
+// var path = document.querySelector('.animated-spiral');
+// var length = path.getTotalLength();
+// // Clear any previous transition
+// path.style.transition = path.style.WebkitTransition =
+//   'none';
+// // Set up the starting positions
+// path.style.strokeDasharray = length + ' ' + length;
+// path.style.strokeDashoffset = length;
+// // Trigger a layout so styles are calculated & the browser
+// // picks up the starting position before animating
+// path.getBoundingClientRect();
+// // Define our transition
+// path.style.transition = path.style.WebkitTransition =
+//   'stroke-dashoffset 2s ease-in-out';
+// // Go!
+// path.style.strokeDashoffset = '0';
+//
+// @keyframes.style.stroke-dashoffset = {
+//   0%   { stroke-dashoffset: 1230;}
+//   50%  { stroke-dashoffset: 0;}
+//   100% { stroke-dashoffset: 1230; }
+// }
